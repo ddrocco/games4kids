af5c786375b9378f8a090768f828298777d83943
ddrocco <keredoccor@gmail.com>
2015-05-11 17:45:28 -0400

    Yay I initialized the unitygit statusgit status

---
+++ Games4Kids/Assets/CharacterControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControl : MonoBehaviour {
+	public Rigidbody body;
+	const float speedScale = 100;
+	const float maxSpeed = 200;
+
+	void Start () {
+		body = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+		if (Input.GetKey(KeyCode.A)) {
+			body.AddForce(Vector3.left * Time.deltaTime * speedScale);
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			body.AddForce(Vector3.down * Time.deltaTime * speedScale);
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			body.AddForce(Vector3.right * Time.deltaTime * speedScale);
+		}
+		if (Input.GetKey(KeyCode.W)) {
+			body.AddForce(Vector3.up * Time.deltaTime * speedScale);
+		}
+		
+		float speed = body.velocity.magnitude;
+		if (speed > maxSpeed) {
+			body.velocity = body.velocity.normalized * maxSpeed;
+		}
+	}
+}
